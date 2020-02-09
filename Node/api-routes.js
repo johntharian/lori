9d9f84c87815e07d1b24d367c1dd2594512de7ff
root <root@LAPTOP-U13VQOH3.localdomain>
2020-02-09 06:13:48 +0530

    node before testing

--- Node/api-routes.js
+++ Node/api-routes.js
@@ -2,11 +2,20 @@
 // Initialize express router
 let router = require('express').Router();
 // Set default API response
-router.get('/', function (req, res) {
-    res.json({
-        status: 'API Its Working',
-        message: 'Welcome to RESTHub crafted with love!'
-    });
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+dat={
+    lat:"00",
+    long:"00"
+};
+
+router.get('/get', function (req, res) {
+    res.json(dat);
 });
+
+router.post('/post', function (req,res){
+        var dat[lat]=req.body.lat;
+        var dat[long]=req.body.long;
+})
 // Export API routes
 module.exports = router;
